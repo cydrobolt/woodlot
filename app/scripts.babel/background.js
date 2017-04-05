343b69314ef42abedb2e1779da52ac77fdc5e42d
Chaoyi Zha <summermontreal@gmail.com>
2017-04-04 21:35:01 -0400

    Implement core features, time tracking

--- app/scripts.babel/background.js
+++ app/scripts.babel/background.js
@@ -1,9 +1,90 @@
-'use strict';
+'use strict'
+/* global chrome, moment */
+
+let activeTree = {
+    // timer: null,
+    // createdTime: null,
+    // grownTime: null
+}
+
+let woodlotTrees = {}
+let updateExtLabelTimer = null
+let minutesLeft = null
+
+function clearExtLabel() {
+    chrome.browserAction.setBadgeText({
+        text: ''
+    })
+}
+
+function setExtLabel() {
+    minutesLeft = activeTree.grownTime.diff(moment(), 'minutes').toString()
+    chrome.browserAction.setBadgeText({
+        text: minutesLeft
+    })
+
+    // alert the popup if it is open
+    chrome.runtime.sendMessage({
+        action: 'treeUpdate',
+        timeLeft: minutesLeft
+    }, function(response) {
+        console.log('tree update: ack')
+    })
+}
+
+function watchExtLabel() {
+    if (updateExtLabelTimer) {
+        clearInterval(updateExtLabelTimer)
+    }
+
+    setExtLabel()
+    updateExtLabelTimer = setInterval(() => {
+        setExtLabel()
+    }, 60000)
+}
 
 chrome.runtime.onInstalled.addListener(details => {
-  console.log('previousVersion', details.previousVersion);
-});
+    console.log('previousVersion', details.previousVersion)
+})
 
-// chrome.browserAction.setBadgeText({text: '\'Allo'});
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        let action = request.action
 
-// console.log('\'Allo \'Allo! Event Page for Browser Action');
+        if (action == 'plantTree') {
+            let treeMins = request.tree.minutes
+
+            activeTree.createdTime = moment()
+            activeTree.grownTime = moment().add(treeMins, 'minutes')
+
+            watchExtLabel()
+
+            activeTree.timer = setTimeout(() => {
+                console.log('Active tree timer elapsed.')
+                chrome.runtime.sendMessage({
+                    action: 'treeGrown',
+                    elapsedTime: activeTree.grownTime.diff(activeTree.createdTime, 'minutes')
+                }, function(response) {
+                    console.log('tree grown: ack')
+                })
+
+                clearInterval(updateExtLabelTimer)
+                clearExtLabel()
+                activeTree = {}
+            }, treeMins * 60 * 1000)
+
+            console.log('sending response')
+            sendResponse({
+                ack: true
+            })
+        } else if (action == 'getTreeInfo') {
+            // Return details on current tree
+            sendResponse({
+                ack: true,
+                timeLeft: minutesLeft,
+                activeTree: activeTree
+            })
+        }
+
+        console.log(sender, request)
+    })
--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,3 +1,81 @@
-'use strict';
+'use strict'
+/* global angular, chrome */
 
-console.log('\'Allo \'Allo! Popup');
+var woodlot = angular.module('woodlot', [])
+
+woodlot.controller('PopupCtrl', ['$scope', ($scope) => {
+    $scope.state = {
+        treeActive: false,
+        timeLeft: null
+    }
+
+    $scope.init = function() {
+        console.log('Loaded PopupCtrl')
+
+        // get tree status
+        chrome.runtime.sendMessage({
+            action: 'getTreeInfo'
+        }, function(response) {
+            let timeLeft = response.timeLeft
+            let treeActive = !$.isEmptyObject(response.activeTree)
+            $scope.state.treeActive = treeActive
+
+            console.log(response)
+
+            $scope.state.timeLeft = timeLeft
+
+            $scope.$digest()
+        })
+
+        // listen for updates from background.js
+        chrome.runtime.onMessage.addListener(
+            function(request, sender, sendResponse) {
+                if (request.action == 'treeGrown') {
+                    let focusedTime = request.elapsedTime
+                    // send a notification to the user
+                    chrome.notifications.create({
+                        iconUrl: 'images/icon-128.png',
+                        type: 'basic',
+                        title: 'Your tree has grown!',
+                        message: 'Your tree has successfully grown. You were focused for ' + focusedTime + ' minutes!'
+                    })
+
+                    $scope.state.timeLeft = null
+                    $scope.state.treeActive = false
+
+                    $scope.$digest()
+                }
+
+                else if (request.action == 'treeUpdate') {
+                    $scope.state.timeLeft = request.timeLeft
+                    $scope.$digest()
+                }
+
+                console.log(sender, request)
+            }
+        )
+    }
+
+    $scope.plantTree = function() {
+        chrome.runtime.sendMessage({
+            action: 'plantTree',
+            tree: {
+                minutes: $('.plant-time').val()
+            }
+        }, function(response) {
+            console.log('received response for plantTree')
+            console.log(response)
+        })
+
+        $scope.state.treeActive = true
+        $scope.$digest()
+    }
+
+    $scope.viewWoodlot = function() {
+
+    }
+
+    $scope.init()
+}])
+
+console.log('Initialized woodlot module')
